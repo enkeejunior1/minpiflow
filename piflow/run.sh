780056ac6604dae49b524c45129b85413e0a5d9a
Yong Hyun Park <park19@login01.cm.cluster>
2025-10-18 17:44:36 -0400

    update code

--- piflow/run.sh
+++ piflow/run.sh
@@ -20,6 +20,5 @@ echo "Job started at $(date)"
 echo "Running on node: $(hostname)"
 echo "Job ID: $SLURM_JOB_ID"
 
-# python3 piflow.py --dataset cifar --NFE 4 --K 8
-# python3 piflow.py --dataset mnist --NFE 4 --K 8 --iter 8 --debug
-python3 piflow.py --dataset mnist --NFE 1 --K 8 --iter 32 --debug
+# python3 piflow.py --dataset mnist --NFE 1 --K 8 --iter 2 --debug 
+python3 piflow.py --dataset cifar --NFE 8 --K 8 --iter 2 --debug 
